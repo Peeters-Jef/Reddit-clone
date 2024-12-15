d448a38223d602c554dd84318c6d0e7b35b448cb
Peeters-Jef <peeters.jef01@gmail.com>
2024-12-15 15:06:33 +0000

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import Nav from './Nav';
+
+function App() {
+    return <div><Nav /></div>;
+}
+
+export default App;
---
+++ src/Nav.tsx
@@ -0,0 +1,14 @@
+function Nav() {
+    return (
+        <nav>
+            <div className="Logo">
+                <a href="#">Reddit</a>
+            </div>
+            <div className="SearchBar">
+                
+            </div>
+        </nav>
+    );
+}
+
+export default Nav;
